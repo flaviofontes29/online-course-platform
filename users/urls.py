d0aa53a0555d05460f5fe16204c5bddc9a7158b5
flaviofontes29 <flavio.carvalho.fontes@gmail.com>
2021-10-05 00:19:10 -0300

    Inclusão de dados na tabela Users

--- users/urls.py
+++ users/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("registration/", views.registration, name="registration"),
     path("login/", views.login, name="login"),
+    path("valid_registry", views.valid_registry, name="valid_registry"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,10 +1,37 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import User
+from django.shortcuts import redirect
+import hashlib
 
 
 def registration(request):
-    return render(request, "registration.html")
+    status = request.GET.get("status")
+    return render(request, "registration.html", {"status": status})
 
 
 def login(request):
     return render(request, "login.html")
+
+
+def valid_registry(request):
+    name = request.POST.get("name")
+    email = request.POST.get("email")
+    password = request.POST.get("password")
+    user_exists = User.objects.filter(email=email)
+
+    if len(name.strip()) == 0 or len(email.strip()) == 0:
+        return redirect("/auth/registration/?status=2")
+
+    if len(user_exists) > 0:
+        return redirect("/auth/registration/?status=1")
+
+    if len(password) < 8 or len(password) > 12:
+        return redirect("/auth/registration/?status=3")
+    try:
+        password = hashlib.sha256(password.encode()).hexdigest()
+        user = User(name=name, email=email, password=password)
+        user.save()
+    except Exception:
+        return redirect("/auth/cadastro/?status=4")
+    return redirect("/auth/registration/?status=0")
